ce6fa4fd23587802db9ab229726ba504cd1d9681
daividvleal <daivid.v.leal@gmail.com>
2024-03-06 22:24:19 -0300

    hello daivd!

--- app/src/main/kotlin/com/example/listview/MainActivity.kt
+++ app/src/main/kotlin/com/example/listview/MainActivity.kt
@@ -8,27 +8,13 @@ import com.example.listview.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val contactList = ArrayList<Person>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        //setUpList()
 
-        Log.e("LOG TYPE:", "ERROR MESSAGE")
-        Log.d("LOG TYPE:", "DEBUG MESSAGE")
-        Log.w("LOG TYPE:", "WARNING MESSAGE")
-        Log.i("LOG TYPE:", "INFORMATION MESSAGE")
-        Log.v("LOG TYPE:", "VERBOSE MESSAGE")
-        //setUpAddNewListElment()
+        binding.helloTxt.text = getString(R.string.hello, "Daivid!")
     }
-
-    /*
-    private fun setUpAddNewListElment() {
-        TODO("Descomente a função e crie a função
-         que adiciona um novo elmento na lista e atualiza a lista.")
-    }*/
-
 
 }
