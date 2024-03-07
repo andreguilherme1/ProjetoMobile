ce6df8da2843caa1222e6c20bfcd38ef2cc7a164
daividvleal <daivid.v.leal@gmail.com>
2024-03-06 22:50:57 -0300

    refactor main

--- app/src/main/java/com/example/networkconection/MainActivity.kt
+++ app/src/main/java/com/example/networkconection/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.networkconection
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.example.hello_world.R
 import com.example.hello_world.databinding.ActivityMainBinding
 
@@ -11,9 +14,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
         binding.helloTxt.text = getString(R.string.hello, "Daivid!")
     }
 
