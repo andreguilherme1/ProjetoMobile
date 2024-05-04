b8ea6cbb5f3d74be2075781380e31e5b681f5ffa
Andre <andreguilherme645@gmail.com>
2024-05-04 18:44:12 -0300

    Adicionando validação nos campos

--- app/src/main/java/com/example/hello_world/MainActivity.kt
+++ app/src/main/java/com/example/hello_world/MainActivity.kt
@@ -10,12 +10,32 @@ import com.example.hello_world.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val contactList = ArrayList<Person>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnAddPerson.setOnClickListener {
+            val name = binding.editTextPersonName.text.toString()
+            val phone = binding.editTextPersonPhone.text.toString()
+            var isToAdd = true
+            if (name.isEmpty()) {
+                binding.editTextPersonName.error =
+                    getString(R.string.message_field_required, getString(R.string.name))
+                isToAdd = false
+            }
+            if (phone.isEmpty()) {
+                binding.editTextPersonPhone.error =
+                    getString(R.string.message_field_required, getString(R.string.phone))
+                isToAdd = false
+            }
+            if (isToAdd) {
+                contactList.add(Person(name, phone))
+            }
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/hello_world/Person.kt
+++ app/src/main/java/com/example/hello_world/Person.kt
@@ -1,3 +1,10 @@
 package com.example.hello_world
 
-data class Person()
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Person(
+    val name: String,
+    val phone: String
+): Parcelable
